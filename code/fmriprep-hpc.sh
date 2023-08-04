777fecfc8b235c8573a16836e4a23649f6c18479
DVSneuro <david.v.smith@temple.edu>
2023-08-04 12:38:35 -0400

    update fmriprep version and add exceptions

--- code/fmriprep-hpc.sh
+++ code/fmriprep-hpc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=16:00:00
+#PBS -l walltime=24:00:00
 #PBS -N fmriprep-all
 #PBS -q normal
 #PBS -m bae
@@ -11,11 +11,9 @@ module load fsl/6.0.2
 source $FSLDIR/etc/fslconf/fsl.sh
 module load singularity/3.8.5
 cd $PBS_O_WORKDIR
-#echo "working in $PBS_O_WORKDIR with OMP_NUM_THREADS=$OMP_NUM_THREADS"
 
-#export OMP_NUM_THREADS=7
 # ensure paths are correct
-maindir=~/work/rf1-mbme-pilot
+maindir=~/work/rf1-data-hpc #this should be the only line that has to change if the rest of the script is set up correctly
 scriptdir=$maindir/code
 bidsdir=$maindir/bids
 logdir=$maindir/logs
@@ -27,12 +25,11 @@ touch $logdir/cmd_fmriprep_${PBS_JOBID}.txt
 
 # make derivatives folder if it doesn't exist.
 # let's keep this out of bids for now
-test=-02
-if [ ! -d $maindir/derivatives$test ]; then
-	mkdir -p $maindir/derivatives$test
+if [ ! -d $maindir/derivatives ]; then
+	mkdir -p $maindir/derivatives
 fi
 
-scratchdir=~/scratch/fmriprep$test
+scratchdir=~/scratch/fmriprep
 if [ ! -d $scratchdir ]; then
 	mkdir -p $scratchdir
 fi
@@ -42,26 +39,42 @@ MPLCONFIGDIR_DIR=~/work/mplconfigdir
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
 export SINGULARITYENV_MPLCONFIGDIR=/opt/mplconfigdir
 
+# need to change this to a more targetted list of subjects
 for sub in `ls -1d $bidsdir/sub-*`; do
-#for sub in sub-10438 sub-10422 sub-10391; do
 	sub="${sub##*/}"
 
-	echo singularity run --cleanenv \
-	-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
-	-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
-	-B $maindir:/base \
-	-B ~/work/tools/licenses:/opts \
-	-B $scratchdir:/scratch \
-	~/work/tools/fmriprep-22.0.2.simg \
-	/base/bids /base/derivatives$test/fmriprep \
-	participant --participant_label $sub \
-	--stop-on-first-crash \
-	--nthreads 12 \
-	--me-output-echos \
-	--stop-on-first-crash \
-	--fs-no-reconall --fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
-
+	if [ $sub -eq 10317 ] || [ $sub -eq 10369 ] || [ $sub -eq 10402 ] || [ $sub -eq 10486 ] || [ $sub -eq 10541 ] || [ $sub -eq 10572 ] || [ $sub -eq 10584 ] || [ $sub -eq 10589 ] || [ $sub -eq 10691 ] || [ $sub -eq 10701 ]; then
+		echo singularity run --cleanenv \
+		-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
+		-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
+		-B $maindir:/base \
+		-B ~/work/tools/licenses:/opts \
+		-B $scratchdir:/scratch \
+		~/work/tools/fmriprep-23.1.3.simg \
+		/base/bids /base/derivatives/fmriprep \
+		participant --participant_label $sub \
+		--stop-on-first-crash \
+		--nthreads 12 \
+		--me-output-echos \
+		--stop-on-first-crash \
+		--use-syn-sdc \
+		--fs-no-reconall --fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+	else
+		echo singularity run --cleanenv \
+		-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
+		-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
+		-B $maindir:/base \
+		-B ~/work/tools/licenses:/opts \
+		-B $scratchdir:/scratch \
+		~/work/tools/fmriprep-23.1.3.simg \
+		/base/bids /base/derivatives/fmriprep \
+		participant --participant_label $sub \
+		--stop-on-first-crash \
+		--nthreads 12 \
+		--me-output-echos \
+		--stop-on-first-crash \
+		--fs-no-reconall --fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+	fi
 done
-# --nthreads 28 --omp-nthreads 7 \
 
 torque-launch -p $logdir/chk_fmriprep_${PBS_JOBID}.txt $logdir/cmd_fmriprep_${PBS_JOBID}.txt
