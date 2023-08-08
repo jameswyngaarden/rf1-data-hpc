f79b953fb6d07749a41f57cfc9d55ff21b7a3f33
David V. Smith <david.v.smith@temple.edu>
2023-08-08 15:25:13 -0400

    specify inputs

--- code/tedana-single.py
+++ code/tedana-single.py
@@ -15,35 +15,27 @@ import time
 import argparse
 
 parser = argparse.ArgumentParser(
-    description='Give me a path to your fmriprep output and number of cores to run')
+    description='Give me a path to your inputs.')
 parser.add_argument('--fmriprepDir',default=None, type=str,help="This is the full path to your fmriprep dir")
 parser.add_argument('--bidsDir',default=None, type=str,help="This is the full path to your BIDS directory")
-parser.add_argument('-sub',default=None, type=int,help="This is the subjectnumber please format with 'sub-###'")
-parser.add_argument('-acq',default=None, type=int,help="This is the acquistion please format with 'acq-###'")
+parser.add_argument('-sub',default=None, type=int,help="This is the subject number.")
+parser.add_argument('-task',default=None, type=str,help="This is the task.")
+parser.add_argument('-runnum',default=None, type=int,help="This is the run number.")
 
 args = parser.parse_args()
-#inputs
 
 prep_data = args.fmriprepDir
 bids_dir=args.bidsDir
 sub=args.sub
-acq=args.acq
+task=args.task
+runnum=args.runnum
 
-#
+
 def RUN_Tedana(sub,prefix,EchoFiles,EchoTimes,OutDir):
-
-    
-    #time.sleep(2)
-    #print(sub,acq+'\n')
-    
-    if os.path.exists("%s/%s_desc-optcomDenoised_bold.nii.gz "%(OutDir,acq)):
-        print('Tedana was previously run for Sub %s acq- %s remove directory if they need to be reanalyzed'%(sub,acq))
-    else:
-  
-        os.makedirs(OutDir,exist_ok=True)
-        print("Running TEDANA for %s %s"%(sub, acq)+'\n')
+       os.makedirs(OutDir,exist_ok=True)
+       print("Running TEDANA for sub-%d_task-%s_run-%d "%(sub, task, runnum)+'\n')
         
-        workflows.tedana_workflow(
+       workflows.tedana_workflow(
 	    EchoFiles,
 	    EchoTimes,
 	    out_dir=OutDir,
@@ -55,19 +47,19 @@ def RUN_Tedana(sub,prefix,EchoFiles,EchoTimes,OutDir):
 
 
 ME_headerinfo=[os.path.join(root, f) for root, dirs, files in os.walk(bids_dir) for f in files
-               if (acq in f)& (f.endswith('_bold.json'))]
+               if (sub in f)& (f.endswith('_bold.json'))]
 echo_times=[json.load(open(f))['EchoTime'] for f in ME_headerinfo]
 echo_times.sort()
          
-acq_image_files=[os.path.join(root, f) for root, dirs, files in os.walk(prep_data) for f in files
-              if (acq in f) & ('echo' in f) & (f.endswith('_desc-preproc_bold.nii.gz'))]
+task_image_files=[os.path.join(root, f) for root, dirs, files in os.walk(prep_data) for f in files
+              if (task in f) & ('echo' in f) & (f.endswith('_desc-preproc_bold.nii.gz'))]
 acq_image_files.sort()
 # # Obtain Echo files
 
 
 out_dir= os.path.join(os.path.abspath(os.path.dirname( prep_data )), "tedana/%s/%s"%(sub,acq))
             
-RUN_Tedana(sub,acq,acq_image_files,echo_times,out_dir):
+RUN_Tedana(sub,acq,acq_image_files,echo_times,out_dir)
 
     
 
